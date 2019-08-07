4e3036601e950c524c2c7c2dac93d449d61f98c3
jaidonlybbert <jaidonlybbert@gmail.com>
2019-08-07 13:35:45 -0700

    Added gravity, bouncing, and friction physics

--- Circles.js
+++ Circles.js
@@ -1,6 +1,16 @@
+// Jaidon Lybbert
+// August 6, 2019
+
 const WIDTH = 800;
 const HEIGHT = 800;
 const GROUND_HEIGHT = 200;
+
+// UNIT CONVERSIONS
+const FPS = 60; // 1 SECOND = 60 FRAMES
+const METER = 120; // 1 METER = 40 PIXELS
+const METER_PER_SECOND = METER / FPS; // 1 M/S = 0.67 PX/FRAME
+const GRAVITY = 9.8 * METER_PER_SECOND / FPS; // 9.8 M/S^2 = 0.11 PX/FRAME^2
+const FRICTION_COEFFICIENT = 0.001;
 
 var canvas = document.getElementById("Circles");
 var ctx = canvas.getContext("2d");
@@ -8,11 +18,20 @@ var ctx = canvas.getContext("2d");
 function Background() {
   this.draw = function() {
     // Sky
-    ctx.fillStyle = "#000";
+    ctx.fillStyle = "#88F";
     ctx.fillRect(0, 0, WIDTH, HEIGHT);
     // Ground
-    ctx.fillStyle = "#666";
+    ctx.fillStyle = "#4F8";
     ctx.fillRect(0, (HEIGHT - GROUND_HEIGHT), WIDTH, GROUND_HEIGHT);
+    // Half-Width distance
+    ctx.fillStyle = "yellow";
+    ctx.fillRect(WIDTH / 4, (HEIGHT - GROUND_HEIGHT), WIDTH / 2, GROUND_HEIGHT / 10);
+    // Person
+    ctx.fillStyle = "blue";
+    ctx.fillRect(300, HEIGHT - METER * 2, 20, METER * 2);
+    // METER STICK
+    ctx.fillstyle = "blue";
+    ctx.fillRect(0, 0, 20, METER);
   }
 }
 
@@ -20,7 +39,8 @@ function Circle() {
   this.size = Math.floor(Math.random() * 11) + 10;
   this.posx = Math.floor(Math.random() * (WIDTH - this.size)) + 1;
   this.posy = Math.floor(Math.random() * (HEIGHT - this.size)) + 1;
-  this.velocity = [0, -10];
+  this.baseline = this.posy;
+  this.velocity = [0, -(METER_PER_SECOND * 5), 0];
 
   this.draw = function() {
     ctx.fillStyle = "#FFF";
@@ -32,6 +52,25 @@ function Circle() {
   this.move = function() {
     this.posx += this.velocity[0];
     this.posy += this.velocity[1];
+  }
+
+  this.accelerate = function() {
+    this.velocity[1] += GRAVITY;
+  }
+
+  this.setPos = function(posx, posy) {
+    this.posx = posx;
+    this.posy = posy;
+    this.baseline = posy;
+  }
+
+  this.setVel = function(velx, vely) {
+    this.velocity[0] = velx;
+    this.velocity[1] = vely;
+  }
+
+  this.setRad = function(radius) {
+    this.size = radius;
   }
 }
 
@@ -46,19 +85,39 @@ function Game() {
     }
   }
 
-  this.loop = function() {
-    console.log("y1");
-    this.c = new Circle();
-    this.circles.push(this.c);
-    console.log(this.circles.length);
+
+  this.physx = function() {
+    // Gravity Acceleration
     for (var i = 0; i < this.circles.length; i++) {
-      console.log("yee");
+      if ((this.circles[i].posy + this.circles[i].velocity[1]) >= this.circles[i].baseline) {
+        if (this.circles[i].velocity[1] < 0.1) {
+          this.circles[i].velocity[1] = 0;
+        } else {
+          this.circles[i].velocity[1] *= -0.9;
+          console.log(this.circles[i].velocity[1]);
+        }
+      } else {
+        this.circles[i].accelerate();
+      }
+
+      // Friction
+      if ((Math.abs(this.circles[i].posy - this.circles[i].baseline) < 2)) {
+        this.circles[i].velocity[0] -= (FRICTION_COEFFICIENT * this.circles[i].velocity[0]);
+      }
+
       this.circles[i].move();
     }
-    this.draw();
   }
 
+  this.loop = function() {
+    this.physx();
+    this.draw();
+  }
 }
 
 var g = new Game();
+var c = new Circle();
+c.setPos(400, 700);
+c.setRad(METER * 0.078 / 2) // Baseball = 78 mm in Diameter
+g.circles.push(this.c);
 setInterval(function() { g.loop(); }, 16.67);
